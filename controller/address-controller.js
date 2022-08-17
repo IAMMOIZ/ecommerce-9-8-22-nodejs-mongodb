4132d3b9d2f6d2d5de980a441311f152c4afff64
Mohasin-ansari10 <mohasin.ansari10@gmail.com>
2022-08-17 10:56:25 +0530

    complete forget password api and otp , nodemailer implimentation

--- controller/address-controller.js
+++ controller/address-controller.js
@@ -1,47 +1,47 @@
-// const addressModel = require("../model/address-model");
+const addressModel = require("../model/address-model");
 
-// //add address
-// const addNewAddress = (req, res) => {
-//   try {
-//     const {
-//       houseNo,
-//       street,
-//       addressLine1,
-//       addressLine2,
-//       city,
-//       state,
-//       country,
-//       pincode,
-//       addressType,
-//       type,
-//     } = req.body;
+//add address
+const addNewAddress = (req, res) => {
+  try {
+    const {
+      houseNo,
+      street,
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      country,
+      pincode,
+      addressType,
+      type,
+    } = req.body;
 
-//     console.log(req.body , "this ----------")
+    console.log(req.body , "this ----------")
 
-//     const address = new addressModel({
-//       houseNo,
-//       street,
-//       addressLine1,
-//       addressLine2,
-//       city,
-//       state,
-//       country,
-//       pincode,
-//       addressType,
-//       type,
-//     });
-//     address.save((err, data) => {
-//       if (err) {
-//         console.log("err");
-//         return res.status(400).json({ msg: "BED REQUEST", error: err });
-//       }
-//       console.log("address added");
-//       return res.status(201).json({ msg: "ADDRESS ADDED", data: data });
-//     });
-//   } catch (error) {
-//     console.log("catch block error", error);
-//     return res.status(500).json({ msg: "SOMETHING WENT WRONG", error: error });
-//   }
-// };
+    const address = new addressModel({
+      houseNo,
+      street,
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      country,
+      pincode,
+      addressType,
+      type,
+    });
+    address.save((err, data) => {
+      if (err) {
+        console.log("err");
+        return res.status(400).json({ msg: "BED REQUEST", error: err });
+      }
+      console.log("address added");
+      return res.status(201).json({ msg: "ADDRESS ADDED", data: data });
+    });
+  } catch (error) {
+    console.log("catch block error", error);
+    return res.status(500).json({ msg: "SOMETHING WENT WRONG", error: error });
+  }
+};
 
-// module.exports = { addNewAddress };
+module.exports = { addNewAddress };
