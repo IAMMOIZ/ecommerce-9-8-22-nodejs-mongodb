6c360e8035df73b5ae5eb50dc14569f4491fe37f
Mohasin-ansari10 <mohasin.ansari10@gmail.com>
2022-08-28 12:13:53 +0530

    otp nodemailer

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
   return res.status(200).send("api is up and working");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
   if (err) {
